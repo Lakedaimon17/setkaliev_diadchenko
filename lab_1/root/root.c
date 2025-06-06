b9f1c948eb2d34d63588b5e1e3ef4cd245a9f806
DamirSM <d.setkaliev@yandex.ru>
2025-06-06 15:56:42 +0600

    fix: float numbers comparison

--- lab_1/root/root.c
+++ lab_1/root/root.c
@@ -7,24 +7,24 @@ int root(double a, double b, double c, double *roots, int *num_roots,
     return ERROR_LINEAR_EQUATION;
   }
   D = b * b - 4 * a * c;
-  if (D < 0) {
+  if (D < -eps) {
     return ERROR_NO_REAL_ROOTS;
   } else if (fabs(D) <= eps) {
     x1 = -b / (2 * a);
-    if (x1 == 0.0) {
+    if (fabs(x1) < eps) {
       x1 = fabs(x1);
     }
     roots[0] = x1;
     *num_roots = 1;
     return 0;
   } else {
-    if (b < 0) {
+    if (b < -eps) {
       x1 = (-b + sqrt(D)) / (2 * a);
     } else {
       x1 = (-b - sqrt(D)) / (2 * a);
     }
     x2 = c / (a * x1);
-    if (x1 > x2) {
+    if (x1-x2 > eps) {
       double temp = x1;
       x1 = x2;
       x2 = temp;
